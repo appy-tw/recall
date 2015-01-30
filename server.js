35480d38b80dbe377be9cb22a70dec6d3f8a0e27
Yuren Ju <yurenju@gmail.com>
2015-01-31 00:46:14 +0800

    publish: Use default port on azure from env variable
    generated from commit 57697327c881ea815fb6e88c8791793432e1d9e1

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var http = require('http');
 var connect = require('connect');
 var url = require('url');
 var proxy = require('proxy-middleware');
+var port = process.env.PORT || 3000;
 
 var app = connect();
 
 app.use('/', connect.static('./app'));
 app.use('/proxy', proxy(url.parse('http://www.ris.gov.tw')));
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(port);
 
