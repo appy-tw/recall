09eb9dc2d3db73ef58ff6d27af1a5a662fd005c8
Mark Chang <mark23456@hotmail.com>
2015-02-07 20:53:22 +0800

    add nav icon

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -18,14 +18,6 @@ angular.module('recallApp')
       return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url+ '?' + (new Date()).getTime()));
     }
 
-    var mapping = {
-      street: '掃街活動',
-      speech: '宣講活動',
-      music: '音樂活動',
-      multiple: '綜合活動',
-      hide: '隱藏活動',
-      boss: '魔王消息'
-    };
 
     parseFeed('http://appytw.tumblr.com/rss').then(function(res) {
       var feeds = []; 
@@ -34,17 +26,6 @@ angular.module('recallApp')
           return;
         }   
         feeds.push(f);
-        angular.forEach(f.categories, function(c) {
-          if (c.indexOf('type:') === 0) {
-            var type = c.substr('type:'.length);
-            f.type = type;
-            f.tag = mapping[type];
-          }   
-        }); 
-        if (!f.tag) {
-          f.tag = '最新消息';
-          f.type = 'multiple';
-        }   
         var d = new Date(f.publishedDate);
         f.date = (d.getYear()+1900) + '/'+ (d.getMonth()+1) + '/' + (d.getDay()+1)
       }); 
